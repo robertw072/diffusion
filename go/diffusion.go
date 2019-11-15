4a30fcf4ae15aa45e9b338b26271445e3a1b5ee1
Robert P. Williamson <williamson_rp@cobra.cs.mercer.edu>
2019-11-15 18:53:36 -0500

    go done

--- go/diffusion.go
+++ go/diffusion.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	const maxsize int = 10
+	const maxsize int = 15 // size of each block
 	var cube[maxsize][maxsize][maxsize] float64
+  // Partition flag 0 off, 1 on
+  var partitionflag int = 1
 
 	// Zero the cube
-	var i, j, k, l, m, n int
+	var i, j, k int
 	for i = 0; i < maxsize; i++ {
 		for j = 0; j < maxsize; j++ {
 			for k = 0; k < maxsize; k++ {
@@ -16,43 +18,137 @@ func main() {
 		}
 	}
 
-	var diffusion_coeff float64 = 0.175
-	var room_dim float64 = 5.0
-	var gas_speed float64 = 250.0
-	var timestep float64 = (room_dim / gas_speed) / float64(maxsize)
-	var block_distance float64 = room_dim / float64(maxsize)
-	var dterm float64 = diffusion_coeff * timestep / (block_distance * block_distance)
+  var partition[maxsize][maxsize][maxsize] float64
+  // If partition is on, zero the partition and construct it
+  if (partitionflag == 1) {
+    for i = 0; i < maxsize; i++ {
+      for j = 0; j < maxsize; j++ {
+        for k = 0; k < maxsize; k++ {
+          partition[i][j][k] = 0.0
+        }
+      }
+    }
+  }
+  // create the actual partition
+  if (partitionflag == 1) {
+    var kp int = maxsize / 2
+    for i = 0; i < maxsize; i++ {
+      for j = maxsize - 1; j > (maxsize / 4); j-- {
+        partition[i][j][kp] = float64(-1)
+      }
+    }
+  }
 
+  	// define variables
+	var diffusionCoeff float64 = 0.175
+	var roomDim float64 = 5.0 // room is 5 by 5 by 5
+	var gasSpeed float64 = 250.0 
+	var timestep float64 = (roomDim / gasSpeed) / float64(maxsize) // used to update simulated time
+	var blockDistance float64 = roomDim / float64(maxsize)
+	var dterm float64 = diffusionCoeff * timestep / (blockDistance * blockDistance) 
+
+  	// initialize first cell of the cube
 	cube[0][0][0] = 1.0e21
 
-	var time float64 = 0.0
-	var ratio float64 = 0.0
+	var time float64 = 0.0 // keeps track of simulated time
+	var ratio float64 = 0.0 // keeps track of termination condition
 
 	for ratio < 0.99 {
 		for i = 0; i < maxsize; i++ {
-		for j = 0; j < maxsize; j++ {
-			for k = 0; k < maxsize; k++ {
-			for l = 0; l < maxsize; l++ {
-				for m = 0; m < maxsize; m++ {
-				for n = 0; n < maxsize; n++ {
-					if (((i==l) && (j==m) && (k==n+1)) || 
-					    ((i==l) && (j==m) && (k==n-1)) || 
-					    ((i==l) && (j==m+1) && (k==n)) || 
-					    ((i==l) && (j==m-1) && (k==n)) || 
-					    ((i==l+1) && (j==m) && (k==n)) || 
-					    ((i==l-1) && (j==m) && (k==n)) ) {
-						var change float64 = (cube[i][j][k] - cube[l][m][n]) * dterm
-						cube[i][j][k] = cube[i][j][k] - change
-						cube[l][m][n] = cube[l][m][n] + change
+			for j = 0; j < maxsize; j++ {
+				for k = 0; k < maxsize; k++ {
+					// repeat for each face of the current cell
+					// if not trying to diffuse into or from the partition, then diffuse
+					if (0 <= k-1 && k-1 < maxsize) {
+						if (partitionflag == 1) {
+							if ((partition[i][j][k] != float64(-1)) && (partition[i][j][k-1] != float64(-1))) {
+								var change float64 = (cube[i][j][k] - cube[i][j][k-1])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j][k-1] = cube[i][j][k-1] + change
+							}
+						} else {
+								var change float64 = (cube[i][j][k] - cube[i][j][k-1])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j][k-1] = cube[i][j][k-1] + change
+							}
 					}
-				}
+
+					if (0 <= k+1 && k+1 < maxsize) {
+						if (partitionflag == 1) {
+							if ((partition[i][j][k] != float64(-1)) && (partition[i][j][k+1] != float64(-1))) {
+								var change float64 = (cube[i][j][k] - cube[i][j][k+1])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j][k+1] = cube[i][j][k+1] + change
+							}
+						} else {
+								var change float64 = (cube[i][j][k] - cube[i][j][k+1])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j][k+1] = cube[i][j][k+1] + change
+							}
+					}
+
+					if (0 <= j-1 && j-1 < maxsize) {
+						if (partitionflag == 1) {
+							if ((partition[i][j][k] != float64(-1)) && (partition[i][j-1][k] != float64(-1))) {
+								var change float64 = (cube[i][j][k] - cube[i][j-1][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j-1][k] = cube[i][j-1][k] + change
+							}
+						} else {
+								var change float64 = (cube[i][j][k] - cube[i][j-1][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j-1][k] = cube[i][j-1][k] + change
+							}
+					}
+
+					if (0 <= j+1 && j+1 < maxsize) {
+						if (partitionflag == 1) {
+							if ((partition[i][j][k] != float64(-1)) && (partition[i][j+1][k] != float64(-1))) {
+								var change float64 = (cube[i][j][k] - cube[i][j+1][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j+1][k] = cube[i][j+1][k] + change
+							}
+						} else {
+								var change float64 = (cube[i][j][k] - cube[i][j+1][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i][j+1][k] = cube[i][j+1][k] + change
+							}
+					}
+
+					if (0 <= i-1 && i-1 < maxsize) {
+						if (partitionflag == 1) {
+							if ((partition[i][j][k] != float64(-1)) && (partition[i-1][j][k] != float64(-1))) {
+								var change float64 = (cube[i][j][k] - cube[i-1][j][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i-1][j][k] = cube[i-1][j][k] + change
+							}
+						} else {
+								var change float64 = (cube[i][j][k] - cube[i-1][j][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i-1][j][k] = cube[i-1][j][k] + change
+							}
+					}
+
+					if (0 <= i+1 && i+1 < maxsize) {
+						if (partitionflag == 1) {
+							if ((partition[i][j][k] != float64(-1)) && (partition[i+1][j][k] != float64(-1))) {
+								var change float64 = (cube[i][j][k] - cube[i+1][j][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i+1][j][k] = cube[i+1][j][k] + change
+							}
+						} else {
+								var change float64 = (cube[i][j][k] - cube[i+1][j][k])*dterm
+								cube[i][j][k] = cube[i][j][k] - change
+								cube[i+1][j][k] = cube[i+1][j][k] + change
+							}
+					}
+
 				}
 			}
-			}
-		}		
 		}
 
-		time = time + timestep
+
+		time = time + timestep // update simulated time
 
 		var maxval float64 = cube[0][0][0]
 		var minval float64 = cube[0][0][0]
@@ -60,18 +156,19 @@ func main() {
 		for i = 0; i < maxsize; i++ {
 			for j = 0; j < maxsize; j++ {
 				for k = 0; k < maxsize; k++ {
+					// determine max and min of cube for ratio update
 					if (cube[i][j][k] > maxval) {
 						maxval = cube[i][j][k]
 					}
-		
-					if (cube[i][j][k] < minval) {
+
+					if ((cube[i][j][k] < minval) && (cube[i][j][k] != 0.0)) {
 						minval = cube[i][j][k]
 					}
 				}
 			}
 		}
 
-		ratio = minval / maxval
+		ratio = minval / maxval // update the ratio
 		fmt.Println(time, " ", ratio)
 
 	}
